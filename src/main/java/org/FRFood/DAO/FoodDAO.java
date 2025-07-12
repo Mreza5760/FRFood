a900bcbc9bbdd3b8935bff86c9d1b53082b16e02
mobin <m.mobin.foroghi.1385@gmail.com>
2025-07-12 22:30:44 +0330

    making it more modular

--- src/main/java/org/FRFood/DAO/FoodDAO.java
+++ src/main/java/org/FRFood/DAO/FoodDAO.java
@@ -4,12 +4,11 @@ import org.FRFood.entity.Food;
 import org.FRFood.util.DataAlreadyExistsException;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public interface FoodDAO{
-//    int insert(Food food) throws DataAlreadyExistsException , SQLException;
-//    Optional<Food> getById(int id);
-//    boolean deleteByID(int id);
+    boolean doesHaveKeyWord(List<String> input) throws SQLException;
 }
 
 
