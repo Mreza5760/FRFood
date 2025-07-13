5106a4ad1490f56688a26afec538150ec2b7ba2a
Mreza5760 <m.reza.576084@gmail.com>
2025-07-13 16:05:02 +0330

    Items version 1

--- src/main/java/org/FRFood/DAO/FoodDAO.java
+++ src/main/java/org/FRFood/DAO/FoodDAO.java
@@ -1,8 +1,12 @@
 package org.FRFood.DAO;
 
+import org.FRFood.entity.Food;
+
 import java.util.List;
+import java.util.Optional;
 import java.sql.SQLException;
 
-public interface FoodDAO{
+public interface FoodDAO {
+    Optional<Food> getById(int id) throws SQLException;
     boolean doesHaveKeywords(List<String> input) throws SQLException;
 }
--- src/main/java/org/FRFood/HTTPHandler/BuyerHandler.java
+++ src/main/java/org/FRFood/HTTPHandler/BuyerHandler.java
@@ -8,6 +8,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.FRFood.util.Authenticate.authenticate;
 
+import org.FRFood.util.BuyerReq.ItemsReq;
 import org.FRFood.util.BuyerReq.VendorsReq;
 
 import java.sql.SQLException;
@@ -31,16 +32,19 @@ public class BuyerHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String path = exchange.getRequestURI().getPath();
         String method = exchange.getRequestMethod();
+        // سوییچ کیس های داخلی هم نیازمند حالت دیفالت هستند
         try {
             switch (method) {
                 case "POST" -> {
                     switch (path) {
                         case "/vendors" -> handleVendorsList(exchange);
+                        case "/items" -> handleItemsList(exchange);
                     }
                 }
                 case "GET" -> {
                     switch (path) {
                         case "/vendors" -> handleVendorsMenu(exchange);
+                        case "/items" -> handleGetItem(exchange);
                     }
                 }
                 case "PUT" -> {
@@ -111,6 +115,61 @@ public class BuyerHandler implements HttpHandler {
             اول اطلاعات رستوران
             بعد لیست اسم منو
             بعد لیست تمام غدا های داخل منو
+             */
+        } catch (SQLException e) {
+//            e.printStackTrace();
+            JsonResponse.sendJsonResponse(exchange, 500, "Database error");
+        }
+    }
+
+    void handleItemsList(HttpExchange exchange) throws IOException {
+        Optional<User> authenticatedUserOptional = authenticate(exchange);
+        if (authenticatedUserOptional.isEmpty()) {
+            return;
+        }
+
+        ItemsReq req = objectMapper.readValue(exchange.getRequestBody(), ItemsReq.class);
+
+        try {
+            List<Food> foodsFiltered = new ArrayList<>();
+            List<Restaurant> restaurants = restaurantDAO.searchByString(req.search);
+            for (Restaurant restaurant : restaurants) {
+                List<Food> foods = restaurantDAO.getFoods(restaurant.getId());
+                for (Food food : foods) {
+                    if (foodDAO.doesHaveKeywords(req.keywords) && food.getPriceId() < req.price) {
+                        foodsFiltered.add(food);
+                    }
+                }
+            }
+            /*
+            اینجا باید لیست غذا های فیلتر شده رو خروجی بدیم
+             */
+        } catch (SQLException e) {
+//            e.printStackTrace();
+            JsonResponse.sendJsonResponse(exchange, 500, "Database error");
+        }
+    }
+
+    void  handleGetItem(HttpExchange exchange) throws IOException {
+        Optional<User> authenticatedUserOptional = authenticate(exchange);
+        if (authenticatedUserOptional.isEmpty()) {
+            return;
+        }
+
+        String path = exchange.getRequestURI().getPath();
+        String[] parts = path.split("/");
+        int id = Integer.parseInt(parts[2]);
+
+        try {
+            Optional<Food> optionalFood = foodDAO.getById(id);
+            if (optionalFood.isEmpty()) {
+                // باید ارور وجود نداشتن رستوران داد
+                return;
+            }
+            Food food = optionalFood.get();
+
+            /*
+            اطلاعات این غذا رو باید پرینت کرد
              */
         } catch (SQLException e) {
 //            e.printStackTrace();
