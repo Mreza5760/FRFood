c7ec1546b4983035a8588df4e7d2d75fb8804697
Mreza5760 <m.reza.576084@gmail.com>
2025-07-15 14:37:15 +0330

    add Rating

---
+++ src/main/java/org/FRFood/entity/Rating.java
@@ -0,0 +1,75 @@
+package org.FRFood.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Rating {
+    private int id;
+    @JsonProperty("user_id")
+    private int userId;
+    private int rating;
+    @JsonProperty("order_id")
+    private int orderId;
+    private String comment;
+    @JsonProperty("created_id")
+    private String createdAt;
+    @JsonProperty("imageBase64")
+    private List<String> images;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+}
