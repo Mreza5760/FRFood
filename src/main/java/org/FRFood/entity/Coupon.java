33159ccb97056df9d297719a1ce1b72225baaad3
Mreza5760 <m.reza.576084@gmail.com>
2025-07-20 17:29:49 +0330

    start Coupon

---
+++ src/main/java/org/FRFood/entity/Coupon.java
@@ -0,0 +1,85 @@
+package org.FRFood.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.FRFood.util.CouponType;
+
+public class Coupon {
+    @JsonProperty("coupon_id")
+    private Integer couponId;
+    @JsonProperty("coupon_code")
+    private String couponCode;
+    private CouponType type;
+    private Integer value;
+    @JsonProperty("min_price")
+    private Integer minPrice;
+    @JsonProperty("user_count")
+    private Integer userCount;
+    @JsonProperty("start_date")
+    private String startDate;
+    @JsonProperty("end_date")
+    private String endDate;
+
+    public Integer getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(Integer couponId) {
+        this.couponId = couponId;
+    }
+
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public void setCouponCode(String couponCode) {
+        this.couponCode = couponCode;
+    }
+
+    public CouponType getType() {
+        return type;
+    }
+
+    public void setType(CouponType type) {
+        this.type = type;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(Integer minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public Integer getUserCount() {
+        return userCount;
+    }
+
+    public void setUserCount(Integer userCount) {
+        this.userCount = userCount;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
