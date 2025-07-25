4fd3180800ae012b15b04b1bcf0d2648de9fa9cf
Mreza5760 <m.reza.576084@gmail.com>
2025-07-25 15:31:19 +0330

    UpdateFoodController

--- src/main/java/org/FRFood/frontEnd/controller/MenuController.java
+++ src/main/java/org/FRFood/frontEnd/controller/MenuController.java
@@ -123,19 +123,17 @@ public class MenuController {
         card.setStyle("-fx-background-color: white; -fx-background-radius: 12; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.1), 10, 0, 0, 6);");
         card.setPrefWidth(600);
 
-        // Logo
         ImageView logo = new ImageView();
         try {
             byte[] imageData = Base64.getDecoder().decode(food.getPicture());
             logo.setImage(new Image(new ByteArrayInputStream(imageData)));
         } catch (Exception e) {
-            logo.setImage(null); // fallback if needed
+            logo.setImage(null);
         }
         logo.setFitWidth(80);
         logo.setFitHeight(80);
         logo.setPreserveRatio(true);
 
-        // Info
         VBox info = new VBox(8);
         info.setAlignment(Pos.CENTER_LEFT);
 
@@ -145,7 +143,7 @@ public class MenuController {
         Label supplyLabel = new Label("📍 Supply: " + food.getSupply());
         supplyLabel.setStyle("-fx-font-size: 14px; -fx-text-fill: #3a3a3a;");
 
-        Label feeLabel = new Label("💰 Price: " + food.getPrice() + "$");
+        Label feeLabel = new Label("💰 Price: " + food.getPrice() + "Toman");
         feeLabel.setStyle("-fx-font-size: 14px; -fx-text-fill: #3a3a3a;");
 
         Label descriptionLabel = new Label("📝 " + food.getDescription());
@@ -159,7 +157,6 @@ public class MenuController {
         HBox rightBox = new HBox();
 
         if (userRole == Role.seller) {
-            // Delete Button
             Button deleteBtn = new Button("Delete");
             deleteBtn.setPrefWidth(100);
             deleteBtn.setPrefHeight(36);
@@ -172,7 +169,6 @@ public class MenuController {
                         -fx-cursor: hand;
                     """);
             deleteBtn.setOnAction(e -> handleDelete(food));
-            // Add both buttons to VBox
             rightBox = new HBox(10, deleteBtn);
             rightBox.setAlignment(Pos.CENTER_RIGHT);
         }
